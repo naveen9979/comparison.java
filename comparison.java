8943b827b654255b09d6f38f9366397fdab226e4
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-06 08:03:26 -0700

    Add files via upload

---
+++ comparison.java
@@ -0,0 +1,41 @@
+public class comparison {
+    public static void main(String[]args){
+        int x = 23;
+        int y = 23;
+        System.out.println(x==y);
+
+
+        int a = 10;
+        int b = 20;
+        if (a != b){
+            System.out.println("a is not equal to b");
+        } else {
+            System.out.println("a is equal to b");
+        }
+
+
+        int u = 100;
+        int o = 45;
+        System.out.println(u > o);
+
+
+        int j = 100;
+        int k = 45;
+        System.out.println(j < k);
+
+
+        int m = 78;
+        int n = 100;
+        if (a <= b)
+        {
+            System.out.println("m is less than or equal to n");
+        }else {
+            System.out.println("m is greater than n");
+        }
+
+
+        
+        
+    }
+    
+}
